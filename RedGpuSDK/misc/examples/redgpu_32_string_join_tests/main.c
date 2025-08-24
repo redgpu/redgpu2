7ed7ade5b3f9f886bd4dc9f66f19a80d1178c091
Constantine Tarasenkov <iamvfx@gmail.com>
2025-08-24 17:49:49 +0300

    Another test for red32StringJoin

--- RedGpuSDK/misc/examples/redgpu_32_string_join_tests/main.c
+++ RedGpuSDK/misc/examples/redgpu_32_string_join_tests/main.c
@@ -9,29 +9,41 @@ void red2Crash(const char * error, const char * functionName, RedHandleGpu optio
 }
 
 int main() {
-  #define STRING_SIZE 13
+  {
+    #define STRING_SIZE 13
 
-  char * str = (char *)red32MemoryCalloc(STRING_SIZE);
-  ((uint64_t *)(void *)str)[0] = red32MirrorBytesOfUint64(STRING_SIZE);
+    char * str = (char *)red32MemoryCalloc(STRING_SIZE);
+    ((uint64_t *)(void *)str)[0] = red32MirrorBytesOfUint64(STRING_SIZE);
 
-  REDGPU_2_EXPECTFL( 0 == red32StringJoin(str, "Hi") );
+    REDGPU_2_EXPECTFL( 0 == red32StringJoin(str, "Hi") );
 
-  REDGPU_2_EXPECTFL( str[0] == 'H' );
-  REDGPU_2_EXPECTFL( str[1] == 'i' );
-  REDGPU_2_EXPECTFL( str[2] == 0   );
-  REDGPU_2_EXPECTFL( ((uint64_t *)(void *)&str[2])[0] == red32MirrorBytesOfUint64(STRING_SIZE) );
+    REDGPU_2_EXPECTFL( str[0] == 'H' );
+    REDGPU_2_EXPECTFL( str[1] == 'i' );
+    REDGPU_2_EXPECTFL( str[2] == 0   );
+    REDGPU_2_EXPECTFL( ((uint64_t *)(void *)&str[2])[0] == red32MirrorBytesOfUint64(STRING_SIZE) );
 
-  REDGPU_2_EXPECTFL( 0 == red32StringJoin(str, "!!!") );
+    REDGPU_2_EXPECTFL( 0 == red32StringJoin(str, "!!!") );
 
-  REDGPU_2_EXPECTFL( str[0] == 'H' );
-  REDGPU_2_EXPECTFL( str[1] == 'i' );
-  REDGPU_2_EXPECTFL( str[2] == '!' );
-  REDGPU_2_EXPECTFL( str[3] == '!' );
-  REDGPU_2_EXPECTFL( str[4] == '!' );
-  REDGPU_2_EXPECTFL( str[5] == 0   );
-  REDGPU_2_EXPECTFL( ((uint64_t *)(void *)&str[5])[0] == red32MirrorBytesOfUint64(STRING_SIZE) );
+    REDGPU_2_EXPECTFL( str[0] == 'H' );
+    REDGPU_2_EXPECTFL( str[1] == 'i' );
+    REDGPU_2_EXPECTFL( str[2] == '!' );
+    REDGPU_2_EXPECTFL( str[3] == '!' );
+    REDGPU_2_EXPECTFL( str[4] == '!' );
+    REDGPU_2_EXPECTFL( str[5] == 0   );
+    REDGPU_2_EXPECTFL( ((uint64_t *)(void *)&str[5])[0] == red32MirrorBytesOfUint64(STRING_SIZE) );
 
-  red32MemoryFree(str);
+    red32MemoryFree(str);
+    #undef STRING_SIZE
+  }
+
+  {
+    char * str = (char *)red32MemoryCalloc(8);
+    ((uint64_t *)(void *)str)[0] = red32MirrorBytesOfUint64(8);
+
+    REDGPU_2_EXPECTFL(  0 == red32StringJoin(str, "")  );
+
+    REDGPU_2_EXPECTFL( -2 == red32StringJoin(str, "!") );
+  }
 
   printf("All tests are passed successfully.\n");
 }
